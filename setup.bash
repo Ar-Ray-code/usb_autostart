cfcaa0028c038d285d425a69c1d32ea50422b4ac
Ar-Ray-code <ray255ar@gmail.com>
2021-02-06 20:41:36 +0900

    update setup.bash

--- setup.bash
+++ setup.bash
@@ -1,9 +1,13 @@
-sudo apt update
-sudo apt upgrade
+#!/bin/bash
+USER_NAME=$(whoami)
+echo ${USER_NAME}
+sudo systemctl set-default multi-user.target
 
-mkdir /home/$(whoami)/usb
-sudo echo -e "[Unit]\nDescripton=USB_Ubuntu Auto Start\nAfter=systemd-networkd-wait-online.service\n\n[Service]\nExecStart=/usr/bin/sudo /bin/sh /home/$(whoami)/.usb_mount.sh\n\nType=simple\n\nUser=ubuntu\nGroup=ubuntu\n\n[Install]\nWantedBy=multi-user.target" >> /etc/systemd/system/usb_ubuntu.service
+mkdir /home/${USER_NAME}/usb
+sudo rm /etc/systemd/system/usb_ubuntu.service
+rm /home/${USER_NAME}/.usb_mount.sh
+sudo sh -c "echo 'source ~/.bashrc\nmount /dev/sda1 /home/${USER_NAME}/usb/\ncd /home/${USER_NAME}/\nsh /home/${USER_NAME}/usb/startup/start_service.sh\nwait' >> /home/${USER_NAME}/.usb_mount.sh"
+sudo sh -c "echo '[Unit]\nDescripton=USB_Ubuntu Auto Start\nAfter=systemd-networkd-wait-online.service\n\n[Service]\nExecStart=/usr/bin/sudo /bin/sh /home/${USER_NAME}/.usb_mount.sh\n\nType=simple\n\nUser=ubuntu\nGroup=ubuntu\n\n[Install]\nWantedBy=multi-user.target' >> /etc/systemd/system/usb_ubuntu.service"
 
-echo -e "source ~/.bashrc\nmount /dev/sda1 /home/$(whoami)/usb/\nsh /home/$(whoami)/usb/startup/start_service.sh\nwait" >> /home/$(whoami)/.usb_mount.sh
 sudo systemctl enable usb_ubuntu.service
 sudo reboot
